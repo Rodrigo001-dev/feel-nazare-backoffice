c06e0bb68dff58eaed5d6560b5b688713c0c7d85
Rodrigo Rael <rodrigorael53@gmail.com>
2025-04-25 14:22:37 -0300

    :wrench: chore: rollback

--- http/get-locais.ts
+++ http/get-locais.ts
@@ -1,29 +1,25 @@
 import type { locais } from '@prisma/client'
 
 import { env } from '@/env'
-import { prisma } from '@/lib/prisma'
 
 export async function getLocais(): Promise<locais[]> {
   try {
-    // const response = await fetch(`${env.NEXT_PUBLIC_API_URL}/locais-concelho`, {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   next: {
-    //     revalidate: 60, // Revalidate every 60 seconds
-    //     tags: ['api/locais-concelho'],
-    //   },
-    // })
+    const response = await fetch(`${env.NEXT_PUBLIC_API_URL}/locais-concelho`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      next: {
+        revalidate: 60, // Revalidate every 60 seconds
+        tags: ['api/locais-concelho'],
+      },
+    })
 
-    // if (!response.ok) {
-    //   throw new Error('Falha ao carregar locais')
-    // }
+    if (!response.ok) {
+      throw new Error('Falha ao carregar locais')
+    }
 
-    // return response.json()
-
-    const locais = await prisma.locais.findMany()
-    return locais
+    return response.json()
   } catch (error) {
     console.error('Erro ao buscar locais:', error)
     throw new Error('Falha ao carregar locais')
